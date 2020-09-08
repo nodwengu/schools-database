8fd2c69eac98513fa3eb819b8d52cc6dbaf4a417
nodwengu <thanduxolonodwengu@gmail.com>
2020-09-08 11:07:51 +0200

    initial commit

--- teacher_subject.sql
+++ teacher_subject.sql
@@ -17,30 +17,30 @@ INSERT INTO teacher_subject(teacher_id, subject_id) VALUES(6, 8);
 INSERT INTO teacher_subject(teacher_id, subject_id) VALUES(1, 8);
 
 
-SELECT *
-FROM teacher AS t
-JOIN teacher_subject AS ts
-ON t.id = ts.teacher_id
-JOIN subject AS s
-ON ts.subject_id = s.id
-WHERE s.name = 'Mathematics';
-
-SELECT s.name, t.first_name
-FROM subject AS s
-JOIN teacher_subject AS ts
-ON s.id = ts.subject_id
-JOIN teacher AS t
-ON ts.teacher_id = t.id
-WHERE t.id = 1;
-
-
-SELECT s.name, t.first_name
-FROM subject AS s
-JOIN teacher_subject AS ts
-ON s.id = ts.subject_id
-JOIN teacher AS t
-ON ts.teacher_id = t.id
-WHERE t.first_name = 'Lindani';
+--SELECT *
+--FROM teacher AS t
+--JOIN teacher_subject AS ts
+--ON t.id = ts.teacher_id
+--JOIN subject AS s
+--ON ts.subject_id = s.id
+--WHERE s.name = 'Mathematics';
+--
+--SELECT s.name, t.first_name
+--FROM subject AS s
+--JOIN teacher_subject AS ts
+--ON s.id = ts.subject_id
+--JOIN teacher AS t
+--ON ts.teacher_id = t.id
+--WHERE t.id = 1;
+--
+--
+--SELECT s.name, t.first_name
+--FROM subject AS s
+--JOIN teacher_subject AS ts
+--ON s.id = ts.subject_id
+--JOIN teacher AS t
+--ON ts.teacher_id = t.id
+--WHERE t.first_name = 'Lindani';
 
 
 
